35aeb5e13a355d89d632810b8f737c94279c9510
Stephan <s0544659@htw-berlin.de>
2020-04-17 22:31:54 +0200

    - init tests for endpoints

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -24,7 +24,13 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+            self.new_question = {
+                'category': 'Anansi Boys',
+                'question': 'Neil Gaiman',
+                'answer': 'haha',
+                'difficulty': 5
+            }
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +39,104 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_given_behavior(self):
+        """Test _____________ """
+        res = self.client().get('/')
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_404_sent_requesting_beyond_valid_page(self):
+        res = self.client().get('/questions?page=1000', json={'difficulty': 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not found')
+
+    # @TODO: Write tests for search - at minimum two
+    #        that check a response when there are results and when there are none
+
+    def test_update_question_difficulty(self):
+        question = Question.query.first()
+        question_id = question.id
+        res = self.client().patch('/questions/' + str(question.id), json={'difficulty': 1})
+        data = json.loads(res.data)
+        question = Question.query.filter(Question.id == question_id).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(question.format()['difficulty'], 1)
+
+    def test_400_for_failed_update(self):
+        res = self.client().patch('/questions/5')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Bad Request')
+
+    def test_delete_question(self):
+        question = Question.query.first()
+        question_id = question.id
+        res = self.client().delete('/questions/' + str(question.id))
+        data = json.loads(res.data)
+
+        question = Question.query.filter(Question.id == question_id).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], question_id)
+        self.assertEqual(data['total_questions'], 0)
+        self.assertEqual(len(data['questions']), 0)
+        self.assertEqual(question, None)
+
+    def test_404_if_question_does_not_exist(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable Entity')
+
+    def test_create_new_question(self):
+        res = self.client().post('/questions', json=self.new_question)
+        data = json.loads(res.data)
+        pass
+
+    def test_422_if_question_creation_fails(self):
+        res = self.client().post('/questions', json=self.new_question)
+        data = json.loads(res.data)
+        pass
+
+    def test_search_question_with_results(self):
+        res = self.client().post('/questions', json={'search': 'Neil'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertEqual(data['total_questions'], 1)
+        self.assertTrue(data['questions'])
+
+    def test_search_question_without_results(self):
+        res = self.client().post('/questions', json={'search': 'hey'})
+        data = json.loads(res.data)
+
+        question = Question.query.filter(self.db.func.lower(Question.question).contains(self.db.func.lower('hey'))).first()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertEqual(data['total_questions'], 0)
+        self.assertEqual(question, None)
+
 
 
 # Make the tests conveniently executable
