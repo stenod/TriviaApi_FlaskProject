f930e6397517d52f86685bfce393e0f4b7c7b763
Stephan <s0544659@htw-berlin.de>
2020-04-19 17:15:34 +0200

    - removed ToDos

--- frontend/src/components/QuizView.js
+++ frontend/src/components/QuizView.js
@@ -49,7 +49,7 @@ class QuizView extends Component {
     if(this.state.currentQuestion.id) { previousQuestions.push(this.state.currentQuestion.id) }
 
     $.ajax({
-      url: '/quizzes', //TODO: update request URL
+      url: '/quizzes',
       type: "POST",
       dataType: 'json',
       contentType: 'application/json',
